5737a2b0551de334c5e68b8914245f65a59a2b8e
yeemh <lkjjr0424@hanmail.net>
2023-06-11 17:17:03 +0900

    ch03 ~.2 study

---
+++ ch03/step_function.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int64)
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = step_function(X)
+plt.plot(X, Y)
+plt.ylim(-0.1, 1.1)  # y축의 범위 지정
+plt.show()
